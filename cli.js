3216846fc3e912ce46a0444a526cf8f52b641b3e
LoveHL <348267823@qq.com>
2016-02-16 08:00:33 +0800

    Update cli.js

--- cli.js
+++ cli.js
@@ -2,7 +2,15 @@ const prog = require('commander');
 const version = require('./package.json').version;
 prog.version(version);
 prog.command('build [dirs...]')
-  .option('--clean', 'clean the dist files;')
-  .option('-o', 'output dir', 'dist')
-  .option('--dev', 'publish the development version.')
-  .option('-w', 'watch the changes');
+  .option('--clean', 'clean output dir before build')
+  .option('--dev', 'build development version')
+  .option('-o, --output <output>', 'set output dir [dist]', 'dist')
+  .option('--verbose', 'output verbose information')
+  .option('-w, --watch', 'rebuild on file change')
+  .action((dirs, options) => {require('./builder').build(dirs, options);});
+
+prog.command('unfreeze').option('-a, --aggresive').action(options => require('./updater').unfreeze(options));
+
+prog.command('freeze').action(() => require('./updater').freeze());
+
+prog.parse(process.argv);
